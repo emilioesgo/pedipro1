ff77ade02b4d918b01c7c4aab1907e9b2447526d
EMILIO ESTRADA GONZALEZ <tu-correo@ejemplo.com>
2026-05-15 22:48:36 -0600

    Actualizacion automatica: 15/05/2026 22:48:36.26

--- functions/index.js
+++ functions/index.js
@@ -9,11 +9,7 @@ const bucket = admin.storage().bucket();
 const FUNCTION_OPTIONS = {
   region: "us-central1",
   invoker: "public",
-  cors: [
-    "https://emilioesgo.github.io",
-    /^http:\/\/127\.0\.0\.1:\d+$/,
-    /^http:\/\/localhost:\d+$/,
-  ],
+  cors: true,
 };
 
 const SUBCOLECCIONES_RESTAURANTE = [
